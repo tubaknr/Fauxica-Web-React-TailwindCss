1c29fb86d2bf4ed6fdcdf985e02a5c65e5fef852
Tuba Çulhaoğlu <tubaculhaoglu1@gmail.com>
2024-09-05 06:23:46 +0300

    dark-mode

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import Sun from "../images/sun.png";
+import Moon from "../images/moon.png";
 import logo from "/home/tuba/Desktop/fauxica-react/fauxica-web/src/images/logo.svg";
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 
 
 export default function Header(){
+    const [isDarkMode, setIsDarkMode] = useState(()=>{
+        const savedTheme = localStorage.getItem("selectedTheme");
+        return savedTheme === "dark";
+    });
+
+    useEffect(()=>{
+        document.querySelector("body").setAttribute("data-theme", isDarkMode ? "dark" : "light");
+        localStorage.setItem('selectedTheme', isDarkMode ? "dark" : "light");
+    },
+    [isDarkMode]);
+    
+    const toggleTheme = (e) => {
+        setIsDarkMode(prevMode => !prevMode);
+    }
+
     return(
         <header className="flex justify-between m-20 mr-30 font-Inter">
             <div class="logo" className="mx-20 ml-20">
@@ -17,6 +34,12 @@ export default function Header(){
                     <li><a href="#" className="text-lg tracking-widest uppercase custom-underline">Products</a></li>
                     <li><a href="#" className="text-lg tracking-widest uppercase custom-underline">Gallery</a></li>
                     <li><Link to="/contact" className="text-lg tracking-widest uppercase custom-underline">Contact</Link></li>
+                    <li>
+                        <button className="bg-none border-0 w-20 mt-[-20px] p-2 cursor-pointer"
+                            onClick={toggleTheme} >
+                            <img src={isDarkMode ? Sun : Moon}/>
+                        </button>
+                    </li>
                 </ul>
             </div>
             <div>
